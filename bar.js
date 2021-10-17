9a6bc3f2f6d3e169a1eca843d33643a4740204ff
Gamesoeazy <mrbin1502@gmail.com>
2021-10-18 02:56:53 +0700

    up

---
+++ bar.js
@@ -0,0 +1,14 @@
+let mu = document.querySelector('.MU');
+function ScrollMu(){
+    mu.scrollIntoView(true);
+}
+
+let mc = document.querySelector('.MC');
+function ScrollMc(){
+    mc.scrollIntoView(true);
+}
+
+let chel = document.querySelector('.Chel');
+function ScrollChel(){
+    chel.scrollIntoView(true);
+}
--- scrollbut.js
+++ scrollbut.js
@@ -1,8 +1,5 @@
-let mybutton = document.getElementById("button");
-
-// When the user scrolls down 20px from the top of the document, show the button
+let mybutton = document.getElementById("Scrollbut");
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -11,8 +8,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
+  document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0;
 }
